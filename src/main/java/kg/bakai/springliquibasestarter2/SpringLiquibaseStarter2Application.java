d1fd6f6b62c25fe27352f4851fd8d770e91f0ff4
Idyrys <sanzharidyrysov@gmail.com>
2024-09-27 16:17:57 +0600

    Liquibase commit number 1

---
+++ src/main/java/kg/bakai/springliquibasestarter2/SpringLiquibaseStarter2Application.java
@@ -0,0 +1,13 @@
+package kg.bakai.springliquibasestarter2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLiquibaseStarter2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLiquibaseStarter2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kg/bakai/springliquibasestarter2/SpringLiquibaseStarter2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.bakai.springliquibasestarter2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLiquibaseStarter2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
